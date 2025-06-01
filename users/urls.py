812bf6e08acd77845fb7e257756f249b6c5cd793
Stiven <69906848+BrayanSRojasG14@users.noreply.github.com>
2025-05-31 23:29:53 -0500

    Add password reset functionality and enhance user templates
    - Added PasswordResetView to handle password reset requests.
    - Updated login.html to include a link for password recovery.
    - Created a new password_reset.html template for the password reset form.
    - Enhanced signup.html with improved styling and layout for better user experience.

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
 from .views import RegisterView
 
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('signup/', RegisterView.as_view(template_name='users/signup.html'), name='signup'),
+    path('password_reset/', PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
 ]
